c992af6e3af41e43c1591b34a3327faf25519855
Madhuchhanda Sahu <madhuchhanda.sahu03@gmail.com>
2025-05-05 22:25:35 -0400

    1stcommit

--- src/test/java/test/TestClass.java
+++ src/test/java/test/TestClass.java
@@ -1,5 +1,39 @@
 package test;
 
-public class TestClass {
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+import driverManager.DriverFactory;
+import scrapper.BaseClass;
+import scrapper.LFV_Elimination;
+import scrapper.Testing;
+
+
+public class TestClass extends BaseClass {
+	DriverFactory driverFactory;
+	BaseClass baseClass;
+	LFV_Elimination elemination;
+	Testing test;
+	
+	@BeforeClass
+	public void initializationSetUp() {
+		driverFactory=new DriverFactory();
+		driverFactory.init_driver("chrome");
+		baseClass=new BaseClass();
+		baseClass.lunchBrowser();
+	}
+	@Test
+	public void getLFVEleminationRecipe() {
+		elemination=new LFV_Elimination(DriverFactory.getDriver());
+		elemination.selectCategory();
+		elemination.getCategory();
+	}
+	@Test
+	public void testScrape() {
+		//driverFactory=new DriverFactory();
+		test=new Testing(DriverFactory.getDriver());
+		test.trial2();
+		
+	}
 
 }
